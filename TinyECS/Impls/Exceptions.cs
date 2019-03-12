6385a86d15101439921c6cfadeb4460db291b452
bnoazx005 <ildar2571@yandex.ru>
2019-03-12 22:50:18 +0400

    Introduce custom exceptions types
    Changes:
    * Two new classes of exceptions ComponentDoesntExistException and
    EntityDoesntExistException were declared.

---
+++ TinyECS/Impls/Exceptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace TinyECS.Impls
+{
+    /// <summary>
+    /// class ComponentDoesntExistException
+    /// 
+    /// The class is an exception type which occurs when someone
+    /// tries to get a component that an entity doesn't have
+    /// </summary>
+
+    public class ComponentDoesntExistException: Exception
+    {
+        public ComponentDoesntExistException(Type type, uint entityId):
+            base($"A component of [{type}] doesn't belong to entity with id [{entityId}]")
+        {
+        }
+    }
+
+
+    /// <summary>
+    /// class EntityDoesntExistException
+    /// 
+    /// The class is an exception's type which occurs when someone 
+    /// ask for an entity which doesn't exist
+    /// </summary>
+
+    public class EntityDoesntExistException: Exception
+    {
+        public EntityDoesntExistException(uint entityId):
+            base($"An entity with the specified identifier [{entityId}] doesn't exist")
+        {
+        }
+    }
+}
