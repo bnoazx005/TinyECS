4b91bb502e5d647496d3a5c0c65e8a6d225209a2
bnoazx005 <ildar2571@yandex.ru>
2019-03-24 19:43:15 +0400

    Declare new interfaces for systems
    Changes:
    * Two new interfaces IInitSystem and IUpdateSystem were declared.

--- TinyECS/Interfaces/ISystem.cs
+++ TinyECS/Interfaces/ISystem.cs
@@ -10,4 +10,37 @@
     public interface ISystem
     {
     }
+
+
+    /// <summary>
+    /// interface IInitSystem
+    /// 
+    /// The interface represents a type of systems that are used only on initialization step of an application
+    /// </summary>
+
+    public interface IInitSystem: ISystem
+    {
+        /// <summary>
+        /// The method is called once on initialization step
+        /// </summary>
+
+        void Init();
+    }
+
+
+    /// <summary>
+    /// interface IUpdateSystem
+    /// 
+    /// The interface represents a type of systems that should be updated every frame
+    /// </summary>
+
+    public interface IUpdateSystem: ISystem
+    {
+        /// <summary>
+        /// The method is called every frame
+        /// </summary>
+        /// <param name="deltaTime">A time which is elapsed from the previous frame</param>
+
+        void Update(float deltaTime);
+    }
 }
