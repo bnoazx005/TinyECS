789501d1c528cedeeab9d62eb5f370b5461a1f62
bnoazx005 <ildar2571@yandex.ru>
2019-04-08 20:36:34 +0400

    Add a new sample project
    Changes:
    * A new sample project which is called SandboxProject was created and
    placed at Samples/ directory.

---
+++ Samples/SandboxProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TinyECS.Impls;
+using TinyECS.Interfaces;
+
+
+namespace SandboxProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWorldContext worldContext = new WorldContext(new EntityManager(new ComponentManager()));
+
+            ISystemManager systemManager = new SystemManager();
+
+            Console.ReadKey();
+        }
+    }
+}
