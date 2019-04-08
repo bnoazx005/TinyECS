57b8d28a02c75007f8f2cae91d0d28d1e4cffd14
bnoazx005 <ildar2571@yandex.ru>
2019-04-08 20:26:46 +0400

    Declare a new interface
    Changes:
    * A new itnerface ISystemsGroup was declared.

---
+++ TinyECS/Interfaces/ISystemsGroup.cs
@@ -0,0 +1,18 @@
+﻿namespace TinyECS.Interfaces
+{
+    /// <summary>
+    /// interface ISystemsGroup
+    /// 
+    /// The interface describes a functionality of a group of systems that can be unified by some criteria
+    /// </summary>
+
+    public interface ISystemsGroup
+    {
+        /// <summary>
+        /// The method adds a new system into the group
+        /// </summary>
+        /// <param name="system">A reference to ISystem implementation</param>
+
+        void Add(ISystem system);
+    }
+}
