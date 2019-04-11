251456866c7d07d211542b2e9896fcb30a9ac15e
bnoazx005 <ildar2571@yandex.ru>
2019-04-11 21:40:55 +0400

    Introduce a concept of pure systems
    Changes:
    * A new concept of pure systems was introduced. A pure system means a
    lambda function which doesn't store its state between calls.
    * A new base class PureSystemAdapter was declared.
    * Two new classes PureInitSystemAdapter and PureUpdateSystemAdapter were
    defined.

--- Samples/SandboxProject/Program.cs
+++ Samples/SandboxProject/Program.cs
@@ -13,8 +13,24 @@ namespace SandboxProject
             
             ISystemManager systemManager = new SystemManager(worldContext);
 
+            systemManager.RegisterInitSystem(new PureInitSystemAdapter(worldContext, (world) =>
+            {
+                // worldContext's variable is available here
+                Console.WriteLine("call Init()");
+            }));
+
+            systemManager.RegisterUpdateSystem(new PureUpdateSystemAdapter(worldContext, (world, dt) =>
+            {
+                // worldContext's variable is available here
+                Console.WriteLine("call Update(float)");
+            }));
+
             systemManager.Init();
-            systemManager.Update(0.0f);
+
+            for (int i = 0; i < 10; ++i)
+            {
+                systemManager.Update(0.0f);
+            }
 
             Console.ReadKey();
         }
---
+++ TinyECS/Impls/PureSystemsAdapters.cs
@@ -0,0 +1,110 @@
+﻿using TinyECS.Interfaces;
+
+
+namespace TinyECS.Impls
+{
+    /// <summary>
+    /// abstract class PureSystemAdapter
+    /// 
+    /// The class is an adapter between lambdas which are pure systems and
+    /// base implementations of systems that are used within the framework
+    /// </summary>
+
+    public abstract class PureSystemAdapter: ISystem
+    {
+        /// <summary>
+        /// The method register the system within the given system manager based on the type
+        /// </summary>
+        /// <param name="systemManager">A reference to ISystemManager implementation</param>
+
+        public abstract void RegisterItself(ISystemManager systemManager);
+    }
+
+
+    /// <summary>
+    /// class PureInitSystemAdapther
+    /// 
+    /// The class is a type of an adapter which is used to utilize lambdas to initialize
+    /// states of entities before the main loop of an application
+    /// </summary>
+
+    public class PureInitSystemAdapter: PureSystemAdapter, IInitSystem
+    {
+        public delegate void InitLambdaSystem(IWorldContext worldContext);
+
+        protected InitLambdaSystem mInitLambdaSystem;
+
+        protected IWorldContext    mWorldContext;
+
+        public PureInitSystemAdapter(IWorldContext worldContext, InitLambdaSystem lambdaSystem)
+        {
+            mWorldContext = worldContext ?? throw new System.ArgumentNullException("worldContext");
+
+            mInitLambdaSystem = lambdaSystem ?? throw new System.ArgumentNullException("lambdaSystem");
+        }
+
+        /// <summary>
+        /// The method register the system within the given system manager based on the type
+        /// </summary>
+        /// <param name="systemManager">A reference to ISystemManager implementation</param>
+
+        public override void RegisterItself(ISystemManager systemManager)
+        {
+            if (systemManager == null)
+            {
+                throw new System.ArgumentNullException("systemManager");
+            }
+
+            systemManager.RegisterInitSystem(this);
+        }
+
+        public void Init()
+        {
+            mInitLambdaSystem?.Invoke(mWorldContext);
+        }
+    }
+
+
+    /// <summary>
+    /// class PureUpdateSystemAdapther
+    /// 
+    /// The class is a type of an adapter which is used to utilize lambdas to update entities
+    /// within the main loop
+    /// </summary>
+
+    public class PureUpdateSystemAdapter : PureSystemAdapter, IUpdateSystem
+    {
+        public delegate void UpdateLambdaSystem(IWorldContext worldContext, float dt);
+
+        protected UpdateLambdaSystem mUpdateLambdaSystem;
+
+        protected IWorldContext      mWorldContext;
+
+        public PureUpdateSystemAdapter(IWorldContext worldContext, UpdateLambdaSystem lambdaSystem)
+        {
+            mWorldContext = worldContext ?? throw new System.ArgumentNullException("worldContext");
+
+            mUpdateLambdaSystem = lambdaSystem ?? throw new System.ArgumentNullException("lambdaSystem");
+        }
+
+        /// <summary>
+        /// The method register the system within the given system manager based on the type
+        /// </summary>
+        /// <param name="systemManager">A reference to ISystemManager implementation</param>
+
+        public override void RegisterItself(ISystemManager systemManager)
+        {
+            if (systemManager == null)
+            {
+                throw new System.ArgumentNullException("systemManager");
+            }
+
+            systemManager.RegisterUpdateSystem(this);
+        }
+
+        public void Update(float dt)
+        {
+            mUpdateLambdaSystem?.Invoke(mWorldContext, dt);
+        }
+    }
+}
