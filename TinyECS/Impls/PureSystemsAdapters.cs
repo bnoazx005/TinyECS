9f9e4310f12e06386903df95da5389d9bea0af2e
bnoazx005 <ildar2571@yandex.ru>
2019-04-17 22:05:19 +0400

    Implement PureReactiveSystemAdapter
    Changes:
    * A new class PureReactiveSystemAdapter was defined.

--- TinyECS/Impls/PureSystemsAdapters.cs
+++ TinyECS/Impls/PureSystemsAdapters.cs
@@ -1,4 +1,5 @@
-﻿using TinyECS.Interfaces;
+﻿using System.Collections.Generic;
+using TinyECS.Interfaces;
 
 
 namespace TinyECS.Impls
@@ -105,6 +106,73 @@ namespace TinyECS.Impls
         public void Update(float dt)
         {
             mUpdateLambdaSystem?.Invoke(mWorldContext, dt);
+        }
+    }
+
+
+    /// <summary>
+    /// class PureReactiveSystemAdapter
+    /// 
+    /// The class is a type of an adapter which is used to utilize lambdas to implement
+    /// reactive system that processes changing entities
+    /// </summary>
+
+    public class PureReactiveSystemAdapter: PureSystemAdapter, IReactiveSystem
+    {
+        public delegate bool ReactiveSystemFilter(IEntity entity);
+
+        public delegate void ReactiveLambdaSystem(IWorldContext worldContext, List<IEntity> entities, float dt);
+        
+        protected ReactiveLambdaSystem mReactiveLambdaSystem;
+
+        protected ReactiveSystemFilter mReactiveSystemFilter;
+
+        protected IWorldContext        mWorldContext;
+
+        public PureReactiveSystemAdapter(IWorldContext worldContext, ReactiveSystemFilter systemFilter, ReactiveLambdaSystem lambdaSystem)
+        {
+            mWorldContext = worldContext ?? throw new System.ArgumentNullException("worldContext");
+
+            mReactiveLambdaSystem = lambdaSystem ?? throw new System.ArgumentNullException("lambdaSystem");
+
+            mReactiveSystemFilter = systemFilter ?? throw new System.ArgumentNullException("systemFilter");
+        }
+
+        /// <summary>
+        /// The method register the system within the given system manager based on the type
+        /// </summary>
+        /// <param name="systemManager">A reference to ISystemManager implementation</param>
+
+        public override void RegisterItself(ISystemManager systemManager)
+        {
+            if (systemManager == null)
+            {
+                throw new System.ArgumentNullException("systemManager");
+            }
+
+            systemManager.RegisterReactiveSystem(this);
+        }
+
+        /// <summary>
+        /// The method filters input list of entities based on its internal predicate's implementation
+        /// </summary>
+        /// <param name="entity">An input entity</param>
+        /// <returns>The method should return true to pass the entity, false in other cases</returns>
+
+        public bool Filter(IEntity entity)
+        {
+            return mReactiveSystemFilter?.Invoke(entity) ?? false;
+        }
+
+        /// <summary>
+        /// The method is called if some entity was changed and passed a filter of the system
+        /// </summary>
+        /// <param name="entities">A list of entities that are passed a filter of the system</param>
+        /// <param name="deltaTime">A time which is elapsed from the previous frame</param>
+
+        public void Update(List<IEntity> entities, float deltaTime)
+        {
+            mReactiveLambdaSystem?.Invoke(mWorldContext, entities, deltaTime);
         }
     }
 }
