76662e4aad58cc19e84c048968a5317a7d2bb351
bnoazx005 <ildar2571@yandex.ru>
2019-04-27 19:29:19 +0400

    Add infrastructure to implement view layer
    Changes:
    * A new class WorldContextsManager was defined.
    * A new static class WorldContextsManagerUtils was defined.
    * A new class DependencyInjection was declared, but has not been defined
    yet.

---
+++ TinyECSUnityIntegration/Impls/DependencyInjector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+namespace TinyECSUnityIntegration.Impls
+{ 
+    /// <summary>
+    /// class DependencyInjector
+    /// 
+    /// The class is a utility class that injects reference to IWorldContext
+    /// </summary>
+
+    public class DependencyInjector: MonoBehaviour
+    {
+    }
+}
---
+++ TinyECSUnityIntegration/Impls/WorldContextsManager.cs
@@ -0,0 +1,48 @@
+﻿using TinyECS.Interfaces;
+using UnityEngine;
+
+
+namespace TinyECSUnityIntegration.Impls
+{
+    /// <summary>
+    /// class WorldContextsManager
+    /// 
+    /// The class represents a manager that provides references to existing worlds contexts.
+    /// NOTE: For now we support the only world context
+    /// </summary>
+
+    public class WorldContextsManager: MonoBehaviour
+    {
+        protected IWorldContext mWorldContext;
+
+        public IWorldContext WorldContext { get => mWorldContext; set => mWorldContext = value; }
+    }
+
+
+    /// <summary>
+    /// class WorldContextsManagerUtils
+    /// 
+    /// The static class contains helper methods to initialize WorldContextsManager
+    /// </summary>
+
+    public static class WorldContextsManagerUtils
+    {
+        /// <summary>
+        /// The extension method returns a new allocated manager of world contexts
+        /// </summary>
+        /// <param name="worldContext"></param>
+        /// <param name="name">A name of a game object that will have WorldContextsManager component</param>
+        /// <returns>The extension method returns a new allocated manager of world contexts</returns>
+
+        public static WorldContextsManager CreateWorldContextManager(this IWorldContext worldContext, string name = null)
+        {
+            GameObject worldContextsManagerGO = new GameObject(name);
+
+            WorldContextsManager worldContextsManager = worldContextsManagerGO.AddComponent<WorldContextsManager>();
+
+            worldContextsManager.WorldContext = worldContext;
+
+            return worldContextsManager;
+        }
+    }
+}
