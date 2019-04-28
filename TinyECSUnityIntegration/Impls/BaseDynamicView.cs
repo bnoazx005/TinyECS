d54961749ba4941cc7283e77cda3913e87c8838b
bnoazx005 <ildar2571@yandex.ru>
2019-04-28 18:39:56 +0400

    Add a new type of a view which is a dynamic view
    Changes:
    * A new class BaseDynamicView was declared.

---
+++ TinyECSUnityIntegration/Impls/BaseDynamicView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+namespace TinyECSUnityIntegration.Impls
+{
+    /// <summary>
+    /// class BaseDynamicView
+    /// 
+    /// The class is a subtype of BaseView which should be used in cases when some view
+    /// will be created in runtime using GameObject.Instantiate or some factory
+    /// </summary>
+    
+    public abstract class BaseDynamicView : BaseView
+    {
+    }
+}
