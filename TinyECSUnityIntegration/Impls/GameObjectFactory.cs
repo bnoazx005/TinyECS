76e39cbfa21f7dde63bc31eea99c566c8c2d6251
bnoazx005 <ildar2571@yandex.ru>
2019-05-01 19:55:19 +0400

    Add an implementation of game objects factory
    Changes:
    * A new interface IGameObjectFactory was declared.
    * A new class GameObjectFactory was implemented.

---
+++ TinyECSUnityIntegration/Impls/GameObjectFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TinyECS.Interfaces;
+using TinyECSUnityIntegration.Interfaces;
+using UnityEngine;
+
+
+namespace TinyECSUnityIntegration.Impls
+{
+    /// <summary>
+    /// class GameObjectFactory
+    /// 
+    /// The class is an implementation of IGameObjectFactory that should be used
+    /// as a replacement of GameObject.Instantiate
+    /// </summary>
+
+    public class GameObjectFactory: IGameObjectFactory
+    {
+        protected IWorldContext mWorldContext;
+
+        public GameObjectFactory(IWorldContext worldContext)
+        {
+            mWorldContext = worldContext ?? throw new ArgumentNullException("worldContext");
+        }
+
+        /// <summary>
+        /// The method instantiates a new instance of a given prefab
+        /// </summary>
+        /// <param name="prefab">A reference to a prefab that should be created dynamically</param>
+        /// <param name="position">A position of a new object</param>
+        /// <param name="rotation">An orientation of an object in a space</param>
+        /// <param name="parent">A parent of a new created object</param>
+        /// <returns>A reference to a new created entity which is attached to a new created GameObject</returns>
+
+        public IEntity Spawn(GameObject prefab, Vector3 position, Quaternion rotation, Transform parent)
+        {
+            GameObject gameObjectInstance = GameObject.Instantiate(prefab, position, rotation, parent);
+
+            IView viewComponent = gameObjectInstance.GetComponent<IView>() ?? throw new NullReferenceException();
+
+            uint linkedEntityId = mWorldContext.CreateEntity(gameObjectInstance.name);
+
+            viewComponent.WorldContext = mWorldContext;
+
+            viewComponent.Link(linkedEntityId);
+            
+            return mWorldContext.GetEntityById(linkedEntityId);
+        }
+    }
+}
---
+++ TinyECSUnityIntegration/Interfaces/IGameObjectFactory.cs
@@ -0,0 +1,28 @@
+﻿using TinyECS.Interfaces;
+using UnityEngine;
+
+
+namespace TinyECSUnityIntegration.Interfaces
+{
+    /// <summary>
+    /// interface IGameObjectFactory
+    /// 
+    /// The interface represents a factory of objects of GameObject type. Use 
+    /// the implementation of this interface should be used as a replacement
+    /// for GameObject.Instantiate invocations
+    /// </summary>
+
+    public interface IGameObjectFactory
+    {
+        /// <summary>
+        /// The method instantiates a new instance of a given prefab
+        /// </summary>
+        /// <param name="prefab">A reference to a prefab that should be created dynamically</param>
+        /// <param name="position">A position of a new object</param>
+        /// <param name="rotation">An orientation of an object in a space</param>
+        /// <param name="parent">A parent of a new created object</param>
+        /// <returns>A reference to a new created entity which is attached to a new created GameObject</returns>
+
+        IEntity Spawn(GameObject prefab, Vector3 position, Quaternion rotation, Transform parent);
+    }
+}
