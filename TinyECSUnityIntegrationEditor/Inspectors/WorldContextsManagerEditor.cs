2ff45d8c66c3f728cf81f0f50bd4759e7868c316
bnoazx005 <ildar2571@yandex.ru>
2019-05-25 21:41:36 +0400

    Add a new subproject TinyECSUnityIntegrationEditor
    Changes:
    * A new sub-project was added into the project which is called
    TinyECSUnityIntegrationEditor.
    * A new class WorldContextManagerEditor of a custom editor was defined.

---
+++ TinyECSUnityIntegrationEditor/Inspectors/WorldContextsManagerEditor.cs
@@ -0,0 +1,15 @@
+﻿using TinyECSUnityIntegration.Impls;
+using UnityEditor;
+
+
+namespace TinyECSUnityIntegrationEditor.Inspectors
+{
+    [CustomEditor(typeof(WorldContextsManager), true)]
+    public class WorldContextsManagerEditor: Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("NOT IMPLEMENTED YET");
+        }
+    }
+}
