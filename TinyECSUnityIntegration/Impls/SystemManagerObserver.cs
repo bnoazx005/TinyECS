a14268e0ab2a18bfd90809ec87a397d8b49879c8
bnoazx005 <ildar2571@yandex.ru>
2019-05-28 22:27:57 +0400

    Add a new helper type for ISystemManager's integration into Unity3D
    Changes:
    * A new class SystemManagerObserver was defined.
    * A new custom editor SystemManagerObserverEditor was defined.
    * A new class SystemManagerObserverUtils was defined.

---
+++ TinyECSUnityIntegration/Impls/SystemManagerObserver.cs
@@ -0,0 +1,48 @@
+﻿using TinyECS.Interfaces;
+using UnityEngine;
+
+
+namespace TinyECSUnityIntegration.Impls
+{
+    /// <summary>
+    /// class SystemManagerObserver
+    /// 
+    /// The class is used for integration of TinyECS with Unity3D and stores a reference to some
+    /// ISystemManager implementation
+    /// </summary>
+
+    public class SystemManagerObserver: MonoBehaviour
+    {
+        protected ISystemManager mSystemManager;
+
+        public ISystemManager SystemManager { get => mSystemManager; set => mSystemManager = value; }
+    }
+
+
+    /// <summary>
+    /// class SystemManagerObserverUtils
+    /// 
+    /// The static class contains helper methods that are used wih SystemManagerObserver
+    /// </summary>
+
+    public static class SystemManagerObserverUtils
+    {
+        /// <summary>
+        /// The method creates a new GameObject which is a representation of ISystemManager within Unity3D's scene
+        /// </summary>
+        /// <param name="systemManager"></param>
+        /// <param name="name">A name of a game object that will have SystemManagerObserver component</param>
+        /// <returns>The extension method returns a new allocated observer of ISystemManager type</returns>
+
+        public static SystemManagerObserver CreateSystemManagerObserver(this ISystemManager systemManager, string name = null)
+        {
+            GameObject systemManagerObserverGO = new GameObject(name);
+
+            SystemManagerObserver systemManagerObserver = systemManagerObserverGO.AddComponent<SystemManagerObserver>();
+
+            systemManagerObserver.SystemManager = systemManager;
+
+            return systemManagerObserver;
+        }
+    }
+}
---
+++ TinyECSUnityIntegrationEditor/Inspectors/SystemManagerObserverEditor.cs
@@ -0,0 +1,23 @@
+﻿using TinyECS.Interfaces;
+using TinyECSUnityIntegration.Impls;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace TinyECSUnityIntegrationEditor.Inspectors
+{
+    [CustomEditor(typeof(SystemManagerObserver), true)]
+    public class SystemManagerObserverEditor: Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            ISystemManager worldContext = (target as SystemManagerObserver)?.SystemManager;
+            
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+
+            EditorGUILayout.LabelField("System Manager Statistics", EditorStyles.boldLabel);
+            
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
