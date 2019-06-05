83dda51a075fc5fd37f28fc485f508042fd779fb
bnoazx005 <ildar2571@yandex.ru>
2019-06-05 22:11:29 +0400

    Add a template project for Unity3D
    Changes:
    * An empty template project for Unity3D was added into /Samples/
    directory.

---
+++ Samples/TinyECSUnity3DTemplate/Assets/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TinyECS.Impls;
+using TinyECS.Interfaces;
+using TinyECSUnityIntegration.Impls;
+
+
+public class Controller: MonoBehaviour
+{
+    protected IWorldContext  mWorldContext;
+
+    protected ISystemManager mSystemManager;
+
+    private void Awake()
+    {
+        mWorldContext = new WorldContextFactory().CreateNewWorldInstance();
+
+        mSystemManager = new SystemManager(mWorldContext);
+
+        WorldContextsManagerUtils.CreateWorldContextManager(mWorldContext, "WorldContextManager_System");
+        SystemManagerObserverUtils.CreateSystemManagerObserver(mSystemManager, "SystemManagerObserver_System");
+
+        mSystemManager.Init();
+    }
+
+    private void Update()
+    {
+        mSystemManager.Update(Time.deltaTime);
+    }
+}
